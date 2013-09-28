627036a0c44e655fb1a87dac8085c54b4b50c0d1
Graham Jackson <graham.jackson07@gmail.com>
2013-09-28 01:08:17 -0400

    initial commit, added test suite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MasterpieceBeersDeux::Application.config.secret_key_base = '862a67e2365585f5dbc2dd9d09b4e66f539641633df16d24c6e0d4c195751a1b568d51ff78a9e7a759a5cd6e3336d969a137b09141d927c4356db05d987b7fb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MasterpieceBeersDeux::Application.config.session_store :cookie_store, key: '_masterpiece_beers_deux_session'
