313c02e058b05b065a4f31a9e3c2d2062e818c97
Graham Jackson <graham.jackson07@gmail.com>
2013-09-30 17:43:35 -0400

    fixed security issue with fb omniauth and fixed avatar issue by incorporating gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def avatar_url(user) 
+  def fb_avatar_url(user) 
     "http://graph.facebook.com/#{user}/picture"
+  end
+
+  def gravatar_url(user)  
+    gravatar_id = Digest::MD5::hexdigest(current_user.email).downcase 
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=50"  
   end 
 end
