03b5f7a9af9f40ee0594adf1651b7acb908a1d2d
kdurr <kristina.l.durr@gmail.com>
2013-10-07 15:23:28 -0400

    additional tests pass and cleaned up show page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,6 +26,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :index_thumb do
     process :resize_to_fill => [226 , 226]
   end
+
+  version :show_thumb do
+    process :resize_to_fill => [300, 300]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
